a277395edc10e09bf19c4e8bf84c477c6c1b6536
Keshav-pro1 <keshavgupta07delhi@gmail.com>
2024-08-27 15:52:39 +0530

    Update program7.c

--- program8.c
+++ program8.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main() {
+    clrscr();
+    int year,check;
+    printf("year= ");
+    scanf("%d",&year);
+    
+    check=year%4;
+    if (check==0){
+        printf("LEAP YEAR");
+    }
+    else{
+         printf("NOT LEAP YEAR");
+    }
+    getch();
+    return 0;
+}
 
