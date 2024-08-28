1fbd848792f407a158dc6633978658ec23d499ea
Keshav-pro1 <keshavgupta07delhi@gmail.com>
2024-08-28 10:58:39 +0000

    This will give size of various datatypes

---
+++ program10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+
+    printf("Size of integer %d \n",sizeof(int));
+    printf("Size of character %d \n",sizeof(char));
+    printf("Size of float %d \n",sizeof(float));
+
+    return 0;
+}
